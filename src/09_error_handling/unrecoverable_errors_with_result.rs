394ead8eec2d526c01474e85a7fe7e595b97d712
25ma <25mateam@gmail.com>
2022-02-14 11:42:07 +0800

    增加rustlt 匹配不同的错误

--- src/09_error_handling/unrecoverable_errors_with_result.rs
+++ src/09_error_handling/unrecoverable_errors_with_result.rs
@@ -1,16 +1,39 @@
 
 use std::fs::File;
+use std::io::ErrorKind;
 fn main() {
     println!("hello,unrecoverable errors with result.");
 
     // result 是一个枚举类型的，它定义有如下两个成员 Ok 和 Err
 
-    let f = File::open("./hello.txt");
+    // let f = File::open("./hello.txt");
 
-    let f = match f {
+    // let f = match f {
+    //     Ok(file) => file,
+    //     Err(error) => panic!("Problem opening the file: {:?}", error),
+    // };
+
+    // println!("f: {:?}", f);
+
+
+    // 匹配不同的错误
+
+    let f2 = File::open("hello.txt");
+
+    let f2 = match f2 {
         Ok(file) => file,
-        Err(error) => panic!("Problem opening the file: {:?}", error),
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(e) => panic!("Problem creating the file : {:?}", e),
+            }, 
+
+            other_error => {
+                panic!("Problem opening the file: {:?}", other_error)
+            }
+        },
     };
 
-    println!("f: {:?}", f);
+    println!("f2: {:?}", f2);
+
 }
